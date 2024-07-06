68e437bf23409841be48bcad1b7b9fbc149074dc
dmshvetsov <shvetsovdm@gmail.com>
2024-07-06 16:08:29 +0200

    init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# code-smell
+
+Reserved package name for the future project
+
+## hint
+
+Use this package to identify bad patterns in the JavaScript and TypeScript code.
+
+## npm
+
+https://www.npmjs.com/package/code-smell
---
+++ index.js
@@ -0,0 +1,4 @@
+throw new Error('not yet implemented')
+
+// feature 1. Monorepo structure smell:
+// identify a bloated package in a monorepo that are used by most of other packages of the monorepo and also imports other packagees the same monorepo
